ce3b68ce4af5f04711c1c88405a2addfc19e388d
Alex Thompson <alexthomps11@gmail.com>
2019-08-26 15:17:06 -0400

    set default path

--- sole-search-app/src/components/Navbar.js
+++ sole-search-app/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import Mens from "./Mens";
@@ -12,8 +12,10 @@ class Navbar extends React.Component {
           <Link to="/">Home</Link>
           <Link to="/mens/">Mens</Link>
         </div>
-        <Route exact path="/" component={Home} />
-        <Route path="/mens/" component={Mens} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/mens/" component={Mens} />
+        </Switch>
       </div>
     );
   }
