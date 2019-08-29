f28527d2ac36b8d125ebe2ee5e712c42b9068c00
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-08-29 13:40:21 -0400

    styled footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
     return (
       <div className="app-container">
         <Header></Header>
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css"
+
+function Footer () {
+    return (
+        <div className="footer">
+            <a href="/">Privacy Policy</a>
+            <a href="/">Terms & Conditions</a>
+        </div>
+    )
+}
+
+export default Footer
